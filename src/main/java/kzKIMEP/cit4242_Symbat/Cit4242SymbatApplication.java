2b67d67fbd0180ff69667aa5b730da918cc3fe1c
Symbat Shalkar <Bout22-29@ntdom.local>
2026-09-01 12:51:26 +0500

    Initialize Maven project

---
+++ src/main/java/kzKIMEP/cit4242_Symbat/Cit4242SymbatApplication.java
@@ -0,0 +1,13 @@
+package kzKIMEP.cit4242_Symbat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cit4242SymbatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cit4242SymbatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kzKIMEP/cit4242_Symbat/Cit4242SymbatApplicationTests.java
@@ -0,0 +1,13 @@
+package kzKIMEP.cit4242_Symbat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cit4242SymbatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
